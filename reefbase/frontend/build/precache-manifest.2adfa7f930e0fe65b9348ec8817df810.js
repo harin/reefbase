028cf06461226cba88d24871aaedd16b4e109df8
Harin Sanghirun <harin.sanghirun@gmail.com>
2019-05-14 21:46:03 -0400

    include frontend build

---
+++ reefbase/frontend/build/precache-manifest.2adfa7f930e0fe65b9348ec8817df810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f8fdbade6e80194df121c4bb8878d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b56162e6c7462bbc1e07",
+    "url": "/static/css/2.c0432ac8.chunk.css"
+  },
+  {
+    "revision": "c218d7420966d8187e49",
+    "url": "/static/css/main.8c22a6a3.chunk.css"
+  },
+  {
+    "revision": "b56162e6c7462bbc1e07",
+    "url": "/static/js/2.e64feb5b.chunk.js"
+  },
+  {
+    "revision": "c218d7420966d8187e49",
+    "url": "/static/js/main.6a7b0281.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
