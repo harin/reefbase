f199e572ea63038742e90ba1c8cff9f642347c17
Harin Sanghirun <harin.sanghirun@gmail.com>
2019-05-27 21:57:30 -0400

    fix routing for spa

--- reefbasedjango/urls.py
+++ reefbasedjango/urls.py
@@ -15,7 +15,8 @@ router.register(r'notes', notes_views.NoteViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url('', include('frontend.urls')),
     url(r'^api/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^(?!api|api-auth|static).*', include('frontend.urls')),
 ]
+
