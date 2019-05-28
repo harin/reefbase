2174ef358ee02791097fb29d79531427facb0f97
Harin Sanghirun <harin.sanghirun@gmail.com>
2019-05-27 20:05:50 -0400

    fix ca path

--- reefbasedjango/settings/production.py
+++ reefbasedjango/settings/production.py
@@ -1,18 +1,17 @@
 from reefbasedjango.settings.common import *
 
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: update this when you have the production host
-ALLOWED_HOSTS = ['reefbase.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'reefbase.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-root_ca_path = BASE_DIR.parent/'amazon-rds-cs-cert.pem'
-print('root_ca_path', root_ca_path)
+root_ca_path = BASE_DIR/'amazon-rds-ca-cert.pem'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -20,9 +19,61 @@ DATABASES = {
         'HOST': os.environ['DATABASE_HOST'] ,
         'USER': os.environ['DATABASE_USER'],
         'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'OPTIONS': { 
-            'sslmode': 'require',
+        'OPTIONS': {
+            'sslmode': 'verify-full',
             'sslrootcert': root_ca_path
         }
     }
 }
+
+# LOGGING = {
+# 	'version': 1,
+# 	'disable_existing_loggers': False,
+# 	'filters': {
+# 		'require_debug_false': {
+# 			'()': 'django.utils.log.RequireDebugFalse',
+# 		},
+# 		'require_debug_true': {
+# 			'()': 'django.utils.log.RequireDebugTrue',
+# 		},
+# 	},
+# 	'formatters': {
+# 		'django.server': {
+# 			'()': 'django.utils.log.ServerFormatter',
+# 			'format': '[%(server_time)s] %(message)s',
+# 		}
+# 	},
+# 	'handlers': {
+# 		'console': {
+# 			'level': 'INFO',
+# 			'filters': ['require_debug_true'],
+# 			'class': 'logging.StreamHandler',
+# 		},
+# 		'console_debug_false': {
+# 			'level': 'ERROR',
+# 			'filters': ['require_debug_false'],
+# 			'class': 'logging.StreamHandler',
+# 		},
+# 		'django.server': {
+# 			'level': 'INFO',
+# 			'class': 'logging.StreamHandler',
+# 			'formatter': 'django.server',
+# 		},
+# 		'mail_admins': {
+# 			'level': 'ERROR',
+# 			'filters': ['require_debug_false'],
+# 			'class': 'django.utils.log.AdminEmailHandler'
+# 		}
+# 	},
+# 	'loggers': {
+# 		'django': {
+# 			'handlers': ['console', 'console_debug_false', 'mail_admins'],
+# 			'level': 'INFO',
+# 		},
+# 		'django.server': {
+# 			'handlers': ['django.server'],
+# 			'level': 'INFO',
+# 			'propagate': False,
+# 		}
+# 	}
+# }
